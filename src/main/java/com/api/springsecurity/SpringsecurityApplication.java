c4ac7ac8dc57c99b23bd5cde9d9ab8c1b46c2f93
ZdcarFI <121211637+zdcarfi@users.noreply.github.com>
2025-06-10 16:19:29 -0500

    Update SpringsecurityApplication.java

--- src/main/java/com/api/springsecurity/SpringsecurityApplication.java
+++ src/main/java/com/api/springsecurity/SpringsecurityApplication.java
@@ -1,14 +1,13 @@
 package com.api.springsecurity;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
+@SpringBootApplication
 public class SpringsecurityApplication {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityApplication.class, args);
 	}
 
-	
 }
