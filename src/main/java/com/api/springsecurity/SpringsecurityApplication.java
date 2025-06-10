c8f1d18c889953568ae5b7fae600521c87e5c6c6
ZdcarFI <121211637+ZdcarFI@users.noreply.github.com>
2025-06-10 16:19:29 -0500

    Update SpringsecurityApplication.java

--- src/main/java/com/api/springsecurity/SpringsecurityApplication.java
+++ src/main/java/com/api/springsecurity/SpringsecurityApplication.java
@@ -1,29 +1,14 @@
 package com.api.springsecurity;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
+@SpringBootTest
 public class SpringsecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringsecurityApplication.class, args);
+	@Test
+	void contextLoads() {
 	}
-	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-			};
-		}
-	}
+
+	
 }
