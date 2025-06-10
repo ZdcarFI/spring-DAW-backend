87b8ce8d4e2d6ed0cc1429740d58f290ff6a5535
carlos0708324 <carlosildefonso.0708@gmail.com>
2025-06-10 16:58:36 -0500

    add url front

--- src/main/java/com/api/springsecurity/config/security/HttpSecurityConfig.java
+++ src/main/java/com/api/springsecurity/config/security/HttpSecurityConfig.java
@@ -90,7 +90,7 @@ public class HttpSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5174"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5174","https://react-daw-frontend.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
